9a1512daa0ec690c1a2e30173580e11f7a2fcc13
luca-bolla <160884195+luca-bolla@users.noreply.github.com>
2026-08-28 16:34:57 -0500

    test.py
    first try at success rate calculators for read and write, not debugged or checked

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
-from fcfd_bridge import FCFD
+from FCFD_I2C_register import FCFD_I2C_register
 
-fcfd = FCFD("./FCFD_I2C_register_map.json")
-fcfd.write_field("clk_eq", 2)
-value = fcfd.read_field("clk40_phase")
-print(value)
+fcfd = FCFD_I2C_register("./FCFD_I2C_register_map.json")
+
+def successful_write_rate(fcfd, register: str, n: int):
+    og_val = fcfd.read(register)
+    success_counter = 0
+    if fcfd._registers[register]['access'] == fcfd.access_type.READ_ONLY: 
+        print(f'Register {register} is read only.')
+        return None
+    for i in range(n):
+        if fcfd.write(register, 0): success_counter+=1
+    fcfd.write(register, og_val)
+    rate = success_counter / n
+    return rate
+
+if __name__ == "__main__":
+    print(successful_write_rate)
